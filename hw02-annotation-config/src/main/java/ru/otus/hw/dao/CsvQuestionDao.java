bf481452b10934cd818a39a475e051e4f63ded75
Matvey0123 <70741384+Matvey0123@users.noreply.github.com>
2025-10-17 17:59:40 +0700

    hw02 done

---
+++ hw02-annotation-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -0,0 +1,35 @@
+package ru.otus.hw.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+import ru.otus.hw.logging.LogIt;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CsvQuestionDao implements QuestionDao {
+
+    private static final String CSV_PARSING_ERROR_MESSAGE = "CSV paring error";
+
+    private final TestFileNameProvider fileNameProvider;
+
+    @LogIt
+    @Override
+    public List<Question> findAll() {
+        List<QuestionDto> questionsFromCsv;
+        try {
+            var csvFileName = fileNameProvider.getTestFileName();
+            var csvReader = CsvToBeanUtil.buildCsvToBean(csvFileName);
+            questionsFromCsv = csvReader.parse();
+        } catch (Exception e) {
+            throw new QuestionReadException(CSV_PARSING_ERROR_MESSAGE, e);
+        }
+
+        return questionsFromCsv.stream().map(QuestionDto::toDomainObject).toList();
+    }
+}
---
+++ hw02-annotation-config/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -0,0 +1,109 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+
+class TestServiceImplTest {
+
+    @InjectMocks
+    private TestServiceImpl testService;
+
+    @Mock
+    private IOService ioService;
+
+    @Mock
+    private QuestionDao questionDao;
+
+    @Mock
+    private QuestionConverter questionConverter;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @DisplayName("Should get test result upon no errors")
+    @Test
+    void shouldGetTestResultUponNoErrors() {
+        var student = new Student("Ivan", "Ivanov");
+        var firstQuestion = new Question("question1", List.of(
+                new Answer("q1 answer1", true),
+                new Answer("q1 answer2", false)));
+        var secondQuestion = new Question("question2", List.of(
+                new Answer("q2 answer1", false),
+                new Answer("q2 answer2", true)));
+        var question1AsString = "question1AsString";
+        var question2AsString = "question2AsString";
+        var questions = List.of(firstQuestion, secondQuestion);
+        when(questionDao.findAll()).thenReturn(questions);
+        when(questionConverter.convertQuestionToString(firstQuestion)).thenReturn(question1AsString);
+        when(questionConverter.convertQuestionToString(secondQuestion)).thenReturn(question2AsString);
+        when(ioService.readIntForRangeWithPrompt(anyInt(), anyInt(), anyString(), anyString())).thenReturn(1);
+
+        var result = testService.executeTestFor(student);
+
+        assertThat(result.getAnsweredQuestions()).containsAll(questions);
+        assertThat(result.getRightAnswersCount()).isEqualTo(1);
+
+        var inOrder = inOrder(questionDao, questionConverter, ioService);
+        inOrder.verify(questionDao).findAll();
+        inOrder.verify(questionConverter).convertQuestionToString(firstQuestion);
+        inOrder.verify(ioService).readIntForRangeWithPrompt(1, 2, question1AsString,
+                "Incorrect answer's number! Please, try again");
+        inOrder.verify(questionConverter).convertQuestionToString(secondQuestion);
+        inOrder.verify(ioService).readIntForRangeWithPrompt(1, 2, question2AsString,
+                "Incorrect answer's number! Please, try again");
+    }
+
+    @DisplayName("Should print error message upon QuestionReadException")
+    @Test
+    void shouldPrintErrorMessageUponQuestionReadException() {
+        var student = new Student("Ivan", "Ivanov");
+        when(questionDao.findAll()).thenThrow(QuestionReadException.class);
+
+        var result = testService.executeTestFor(student);
+
+        assertThat(result.getAnsweredQuestions()).isEmpty();
+        assertThat(result.getRightAnswersCount()).isEqualTo(0);
+
+        verify(ioService).printLine("Error during generating questions. Sorry, testing is cancelled");
+        verifyNoInteractions(questionConverter);
+        verify(ioService, never()).readIntForRangeWithPrompt(anyInt(), anyInt(), anyString(), anyString());
+    }
+
+    @DisplayName("Should get wrong answer exception during get answer")
+    @Test
+    void shouldGetWrongAnswerUponExceptionDuringGetAnswer() {
+        var student = new Student("Ivan", "Ivanov");
+        var question = new Question("question1", List.of(
+                new Answer("q1 answer1", true),
+                new Answer("q1 answer2", true)));
+        when(questionDao.findAll()).thenReturn(List.of(question));
+        when(ioService.readIntForRangeWithPrompt(anyInt(), anyInt(), anyString(), anyString()))
+                .thenThrow(IllegalArgumentException.class);
+
+        var result = testService.executeTestFor(student);
+
+        assertThat(result.getAnsweredQuestions()).containsExactly(question);
+        assertThat(result.getRightAnswersCount()).isEqualTo(0);
+
+        verify(questionDao).findAll();
+        verify(ioService).printLine("Attempts to answer the question have expired. Answer is considered to be wrong");
+    }
+
+}
